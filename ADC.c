7f914b43e53aadebc7823ea66fbb965fbf8439cd
Albertoie710231 <ie710231@iteso.mx>
2020-05-16 00:54:04 -0500

    Added ADC modules

---
+++ ADC.c
@@ -0,0 +1,137 @@
+/*
+ * ADC.c
+ *
+ *  Created on: 9 nov 2019
+ *      Author: alber
+ */
+
+#include "ADC.h"
+#include "MK64F12.h"
+
+void ADC_clk_gating(adc_t adc_n)
+{
+	switch(adc_n)
+	{
+	case ADC_0:
+		SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK;
+		break;
+	case ADC_1:
+		SIM->SCGC3 |= SIM_SCGC3_ADC1_MASK;
+		break;
+	}
+}
+
+void ADC_input_chn_select(adc_t adc_n, adc_scn_x_t sc1n_x, adc_channel_t channel)
+{
+	switch(adc_n)
+	{
+	case ADC_0:
+		ADC0->SC1[sc1n_x] = ADC_SC1_ADCH(channel);
+		break;
+	case ADC_1:
+		ADC1->SC1[sc1n_x] = ADC_SC1_ADCH(channel);
+		break;
+	}
+}
+
+void ADC_differential_mode(adc_t adc_n, adc_scn_x_t sc1n_x,adc_diff_mode_t diff_mode)
+{
+	switch(adc_n)
+	{
+	case ADC_0:
+		if(ENB_DIFF == diff_mode)
+			ADC0->SC1[sc1n_x] |= ADC_SC1_DIFF_MASK;
+		else if(DES_DIFF == diff_mode)
+			ADC0->SC1[sc1n_x] &= ~ADC_SC1_DIFF_MASK;
+		break;
+	case ADC_1:
+		if(ENB_DIFF == diff_mode)
+			ADC1->SC1[sc1n_x] |= ADC_SC1_DIFF_MASK;
+		else if(DES_DIFF == diff_mode)
+			ADC1->SC1[sc1n_x] &= ~ADC_SC1_DIFF_MASK;
+		break;
+	}
+}
+
+uint16_t ADC_data_result(adc_t adc_n, adc_scn_x_t sc1_n)
+{
+	uint16_t data_result = 0;
+	switch(adc_n)
+	{
+	case ADC_0:
+		while(0 == (ADC0->SC1[sc1_n] & ADC_SC1_COCO_MASK));
+		data_result = (ADC0->R[sc1_n]);
+		break;
+	case ADC_1:
+		while(0 == (ADC1->SC1[sc1_n] & ADC_SC1_COCO_MASK));
+		data_result = 0xFF & (ADC1->R[sc1_n]);
+		break;
+	}
+	return (data_result);
+}
+
+void ADC_conversion_mode(adc_t adc_n, adc_clk_devide_t clk_devide, adc_mode_t resolution_mode, adc_input_clk_t imput_clk)
+{
+	switch(adc_n)
+	{
+	case ADC_0:
+		ADC0->CFG1 |= ADC_CFG1_ADIV(clk_devide);
+		ADC0->CFG1 |= ADC_CFG1_MODE(resolution_mode);
+		ADC0->CFG1 |= ADC_CFG1_ADICLK(imput_clk);
+		break;
+	case ADC_1:
+		ADC1->CFG1 |= ADC_CFG1_ADIV(clk_devide);
+		ADC1->CFG1 |= ADC_CFG1_MODE(resolution_mode);
+		ADC1->CFG1 |= ADC_CFG1_ADICLK(imput_clk);
+		break;
+	}
+}
+
+void ADC_calibration(adc_t adc_n, hwd_average_t average)
+{
+	switch(adc_n)
+	{
+	case ADC_0:
+		ADC0->SC3 |= ADC_SC3_AVGE_MASK;
+		ADC0->SC3 |= ADC_SC3_AVGS(average);
+		break;
+	case ADC_1:
+		ADC1->SC3 |= ADC_SC3_AVGE_MASK;
+		ADC1->SC3 |= ADC_SC3_AVGS(average);
+		break;
+	}
+}
+
+void ADC_sample_time(adc_t adc_n, adc_sample_time_t sample_time)
+{
+	switch(adc_n)
+	{
+	case ADC_0:
+		if(SHORT_SAMPLE == sample_time)
+			ADC0->CFG1 &= ~ADC_CFG1_ADLSMP_MASK;
+		else if(LONG_SAMPLE == sample_time)
+			ADC0->CFG1 |= ADC_CFG1_ADLSMP_MASK;
+		break;
+	case ADC_1:
+		if(SHORT_SAMPLE == sample_time)
+			ADC1->CFG1 &= ~ADC_CFG1_ADLSMP_MASK;
+		else if(LONG_SAMPLE == sample_time)
+			ADC1->CFG1 |= ADC_CFG1_ADLSMP_MASK;
+		break;
+	}
+}
+
+void ADC_init(const adc_config_t* config_struct)
+{
+	ADC_clk_gating(config_struct->adc);
+
+	ADC_input_chn_select(config_struct->adc, config_struct->scn_x,config_struct->channel);
+
+	ADC_differential_mode(config_struct->adc, config_struct->scn_x, config_struct->diff_mode);
+
+	ADC_conversion_mode(config_struct->adc, config_struct->clk_div, config_struct->adc_res, config_struct->imput_clk);
+
+	ADC_calibration(config_struct->adc,config_struct->avrg);
+
+	ADC_sample_time(config_struct->adc, config_struct->smpl_time);
+}
---
+++ ADC.h
@@ -0,0 +1,126 @@
+/*
+ * ADC.h
+ *
+ *  Created on: 9 nov 2019
+ *      Author: alber
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "MK64F12.h"
+
+typedef enum{
+	 ADC_0,
+	 ADC_1
+}adc_t;
+
+typedef enum{
+	 SCn_A,
+	 SCn_B
+}adc_scn_x_t;
+
+typedef enum
+{
+	DIV_1,
+	DIV_2,
+	DIV_4,
+	DIV_8
+}adc_clk_devide_t;
+
+typedef enum
+{
+	DES_DIFF,
+	ENB_DIFF
+}adc_diff_mode_t;
+
+typedef enum
+{
+	SHORT_SAMPLE,
+	LONG_SAMPLE
+}adc_sample_time_t;
+
+typedef enum
+{
+	MODE_0,
+	MODE_1,
+	MODE_2,
+	MODE_3
+}adc_mode_t;
+
+typedef enum
+{
+	BUS_CLK,
+	ALTCLK2,
+	ALTCLK,
+	ADACK
+}adc_input_clk_t;
+
+typedef enum
+{
+	DAD_0,
+	DAD_1,
+	DAD_2,
+	DAD_3,
+	AD_4,
+	AD_5,
+	AD_6,
+	AD_7,
+	AD_8,
+	AD_9,
+	AD_10,
+	AD_11,
+	AD_12,
+	AD_13,
+	AD_14,
+	AD_15,
+	AD_16,
+	AD_17,
+	AD_18,
+	AD_19,
+	AD_20,
+	AD_21,
+	AD_22,
+	AD_23
+}adc_channel_t;
+
+typedef enum
+{
+	SAMP_4,
+	SAMP_8,
+	SAMP_16,
+	SAMP_32
+}hwd_average_t;
+
+typedef struct
+{
+	adc_t adc;
+	adc_scn_x_t scn_x;
+	hwd_average_t avrg;
+	adc_diff_mode_t diff_mode;
+	adc_channel_t channel;
+	adc_clk_devide_t clk_div;
+	adc_sample_time_t smpl_time;
+	adc_mode_t adc_res;
+	adc_input_clk_t imput_clk;
+} adc_config_t;
+
+
+void ADC_clk_gating(adc_t adc_n);
+
+void ADC_input_chn_select(adc_t adc_n, adc_scn_x_t sc1n_x, adc_channel_t channel);
+
+void ADC_differential_mode(adc_t adc_n, adc_scn_x_t sc1n_x,adc_diff_mode_t diff_mode);
+
+uint16_t ADC_data_result(adc_t adc_n, adc_scn_x_t sc1_n);
+
+void ADC_conversion_mode(adc_t adc_n, adc_clk_devide_t clk_devide, adc_mode_t resolution_mode, adc_input_clk_t imput_clk);
+
+void ADC_sample_time(adc_t adc_n, adc_sample_time_t sample_time);
+
+void ADC_calibration(adc_t adc_n, hwd_average_t average);
+
+void ADC_init(const adc_config_t* config_struct);
+
+
+#endif /* ADC_H_ */
